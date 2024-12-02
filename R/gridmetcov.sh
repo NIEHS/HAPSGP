f861503b57b24a46d6ec00c974cf1a0307859877
MAKassien <marianaalifa@gmail.com>
2024-12-01 23:54:59 -0500

    agu dev

--- R/gridmetcov.sh
+++ R/gridmetcov.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#SBATCH --job-name=gridmetcov
-#SBATCH --error=gridmetcov.error
+#SBATCH --job-name=gridmetcov2
+#SBATCH --error=gridmetcov2.error
 #SBATCH --mail-user=kassienma@nih.gov
 #SBATCH --mail-type=START,END,FAIL
 #SBATCH --ntasks=1
 
 Rscript covariates_processing.R
-
