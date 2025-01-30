2a841bd2db4660c949d30b22a8723b4850fb925b
MAKassien <marianaalifa@gmail.com>
2025-01-30 12:03:06 -0500

    update fit function

--- R/pgp_fit.R
+++ R/pgp_fit.R
@@ -22,10 +22,10 @@ df <- df %>%
   mutate(CONC_DAILY_STD = if_else(CONC_DAILY_STD <= 0, MDL_DAILY_STD_UG_M3 / 20, CONC_DAILY_STD)) %>%
   filter(CONC_DAILY_STD < 300 )
 
-# Change NA's in MDL (not many occurences of this)
+# Change NA's or <=0s in MDL 
 df <- df %>%
   group_by(AQS_PARAMETER_CODE) %>%
-  mutate(MDL_DAILY_STD_UG_M3 = if_else(MDL_DAILY_STD_UG_M3 <= 0,
+  mutate(MDL_DAILY_STD_UG_M3 = if_else(MDL_DAILY_STD_UG_M3 <= 0 | is.na(MDL_DAILY_STD_UG_M3),
                                        min(CONC_DAILY_STD[CONC_DAILY_STD > 0], na.rm = TRUE) / 10,
                                        MDL_DAILY_STD_UG_M3)) %>%
   ungroup()
